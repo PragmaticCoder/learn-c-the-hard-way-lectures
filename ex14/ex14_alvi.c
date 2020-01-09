091b9ca3881a4ff3958cb8f70e64e87dfbcccc18
Alvi Akbar <ala273@tux7-1.usask.ca>
2020-01-09 04:08:57 -0600

    minor code formatting

--- ex14/ex14_alvi.c
+++ ex14/ex14_alvi.c
@@ -1,41 +1,35 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 // forward declarations
 int can_print_it(char ch);
 void print_letters(char arg[]);
 
-void print_arguments(int argc, char *argv[])
-{
-    int i = 0;
+void print_arguments(int argc, char *argv[]) {
+  int i = 0;
 
-    for (i = 0; i < argc; i++) {
-        print_letters(argv[i]);
+  for (i = 0; i < argc; i++) {
+    print_letters(argv[i]);
+  }
+}
+
+void print_letters(char arg[]) {
+  int i = 0;
+
+  for (i = 0; arg[i] != '\0'; i++) {
+    char ch = arg[i];
+
+    if (can_print_it(ch)) {
+      printf("'%c' == %d ", ch, ch);
     }
+  }
+
+  printf("\n");
 }
 
-void print_letters(char arg[])
-{
-    int i = 0;
+int can_print_it(char ch) { return isalpha(ch) || isblank(ch); }
 
-    for (i = 0; arg[i] != '\0'; i++) {
-        char ch = arg[i];
-
-        if (can_print_it(ch)) {
-            printf("'%c' == %d ", ch, ch);
-        }
-    }
-
-    printf("\n");
-}
-
-int can_print_it(char ch)
-{
-    return isalpha(ch) || isblank(ch);
-}
-
-int main(int argc, char *argv[])
-{
-    print_arguments(argc+1, argv);
-    return 0;
+int main(int argc, char *argv[]) {
+  print_arguments(argc + 1, argv);
+  return 0;
 }
