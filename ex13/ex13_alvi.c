5b4ce5ed942bf1b363d100db698932d0502b7283
Alvi Akbar <ala273@tux7-1.usask.ca>
2020-01-09 03:52:08 -0600

    minor code formatting

--- ex13/ex13_alvi.c
+++ ex13/ex13_alvi.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-    int i = 0;
+int main(int argc, char *argv[]) {
+  int i = 0;
 
-    // go through each string in argv
-    // why am I skipping argv[0]?
-    for (i = 0; i < argc; i++) {
-        printf("arg %d: %s\n", i, argv[i]);
-    }
+  // go through each string in argv
+  // why am I skipping argv[0]?
+  for (i = 0; i < argc; i++) {
+    printf("arg %d: %s\n", i, argv[i]);
+  }
 
-    // let's make our own array of strings
-    char *states[] = {
-        "California", "Oregon",
-        "Washington", "Texas"
-    };
+  // let's make our own array of strings
+  char *states[] = {"California", "Oregon", "Washington", "Texas"};
 
-    int num_states = 5;
+  int num_states = 5;
 
-    for (i = 0; i < num_states; i++) {
-        printf("state %d: %s\n", i, states[i]);
-    }
+  for (i = 0; i < num_states; i++) {
+    printf("state %d: %s\n", i, states[i]);
+  }
 
-    return 0;
+  return 0;
 }
