d03dde576eab87ebd305f0377b203ca504871999
Alvi Akbar <ala273@tux6-1.usask.ca>
2020-01-12 08:35:41 -0600

    uncommented tests

--- ex19/ex19_alvi.c
+++ ex19/ex19_alvi.c
@@ -90,19 +90,19 @@ error:
 }
 
 int main(int argc, char *argv[]) {
-  // check(argc == 2, "Need an argument.");
+  check(argc == 2, "Need an argument.");
 
   test_debug();
-  // test_log_err();
-  // test_log_warn();
-  // test_log_info();
+  test_log_err();
+  test_log_warn();
+  test_log_info();
 
-  // check(test_check("ex20.c") == 0, "failed with ex20.c");
-  // check(test_check(argv[1]) == -1, "failed with argv");
-  // check(test_sentinel(1) == 0, "test_sentinel failed.");
-  // check(test_sentinel(100) == -1, "test_sentinel failed.");
-  // check(test_check_mem() == -1, "test_check_mem failed.");
-  // check(test_check_debug() == -1, "test_check_debug failed.");
+  check(test_check("ex20.c") == 0, "failed with ex20.c");
+  check(test_check(argv[1]) == -1, "failed with argv");
+  check(test_sentinel(1) == 0, "test_sentinel failed.");
+  check(test_sentinel(100) == -1, "test_sentinel failed.");
+  check(test_check_mem() == -1, "test_check_mem failed.");
+  check(test_check_debug() == -1, "test_check_debug failed.");
 
   return 0;
 
