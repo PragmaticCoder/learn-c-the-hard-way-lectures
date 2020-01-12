697fc202295c5b6581a317ade27cfe5944c7f687
Alvi Akbar <ala273@tux6-1.usask.ca>
2020-01-12 00:56:14 -0600

    finished implementation Database_open

--- ex17/ex17_alvi.c
+++ ex17/ex17_alvi.c
@@ -33,9 +33,44 @@ void die(const char *message) {
   exit(1);
 }
 
+void Database_load(struct Connection *conn) {
+  printf("Got to Database_load function");
+}
+
+struct Connection *Database_open(const char *filename, const char mode) {
+
+  struct Connection *conn = malloc(sizeof(struct Connection));
+  if (!conn)
+    die("Memory Error");
+
+  conn->db = malloc(sizeof(struct Database));
+  if (!conn->db)
+    die("Memory error");
+
+  if (mode == 'c')
+    conn->file = fopen(filename, "w");
+  else
+    conn->file = fopen(filename, "r");
+
+  if (conn->file) {
+    Database_load(conn);
+  }
+
+  if (!conn->file)
+    die("Failed to open the file");
+
+  return conn;
+}
+
 int main(int argc, char *argv[]) {
+
   if (argc < 3)
     die("USAGE: ex17 <dbfile> <action> [action params]");
+
+  char *filename = argv[1];
+  char action = argv[2][0];
+
+  struct Connection *conn = Database_open(filename, action);
 
   return 0;
 }
