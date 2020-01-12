1ce1c8410d925da6e116699ec1f0312dada83595
Alvi Akbar <ala273@tux6-1.usask.ca>
2020-01-12 02:03:18 -0600

    wrote down the skeleton program

--- ex18/ex18_alvi.c
+++ ex18/ex18_alvi.c
@@ -23,5 +23,16 @@ int main(int argc, char *argv[]) {
   if (argc < 2)
     die("USAGE: ex18_alvi 4 3 1 5 6");
 
+  int count = argc - 1;
+  int i = 0;
+  char **inputs = argv + 1;
+
+  int *numbers = malloc(count * sizeof(int));
+  if (!numbers)
+    die("Memory Error");
+  
+  for(i = 0; i < count; i++)
+    numbers[i] = atoi(inputs[i]);
+
   return 0;
 }
